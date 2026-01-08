c103dc599cacaf7fe8b8e5287bf0e62811e4d3ed
Chris Cai <xinyaocai1228@gmail.com>
2026-01-08 13:29:05 -0600

    Add signals, update rules, and learning engine

--- coupled_learning/__init__.py
+++ coupled_learning/__init__.py
@@ -4,9 +4,18 @@ __version__ = "0.1.0"
 
 from .backends import PhysicsBackend
 from .simple_backend import SimpleMechanicalBackend, SimpleFlowBackend
+from .signals import SignalType, SignalComputer, create_3x3_patch_sampling
+from .update_rules import UpdateRule, create_update_rule
+from .engine import CoupledLearningEngine
 
 __all__ = [
     'PhysicsBackend',
-    'SimpleMechanicalBackend', 
+    'SimpleMechanicalBackend',
     'SimpleFlowBackend',
+    'SignalType',
+    'SignalComputer',
+    'create_3x3_patch_sampling',
+    'UpdateRule',
+    'create_update_rule',
+    'CoupledLearningEngine',
 ]
